f3d6c506de7aedce71399582f38f38e4d733df46
Nkosi Phillip <internap627@gmail.com>
2018-09-15 03:37:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,9 @@ def summon_captain_planet(array)
   array.map {|word| word.capitalize+"!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  array.any? {|word| word.size > 4}
 end
 
 def find_the_cheese# code an argument here
