6fb71c8b045bcf8f1527beec3c025fa5a26f2ccd
Nkosi Phillip <internap627@gmail.com>
2018-09-15 03:59:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,19 @@ def long_planeteer_calls(array)
   array.any? {|word| word.size > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  count = 0
+
+  while count < cheese_types.size
+  newArr = array.select {|food| food == cheese_types[count]}
+  if newArr.size > 0
+  return newArr.join()
+  end
+  count += 1
+  end
+
+  return nil
+
 end
